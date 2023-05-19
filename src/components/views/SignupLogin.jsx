a7b763a654651c78d89900734bdcbb2e57c62990
ClaesF0 <claes_folkestad@hotmail.com>
2023-05-20 01:04:36 +0200

    signup login form validation. to do: APIcall

--- src/components/views/SignupLogin.jsx
+++ src/components/views/SignupLogin.jsx
@@ -23,11 +23,6 @@ const validationSchema = Yup.object().shape({
   confirmPassword: Yup.string()
     .oneOf([Yup.ref("password"), null], "Passwords must match")
     .required("Please confirm your password"),
-
-  //message: Yup.string()
-  //.matches(/^[a-zæøå0-9]+$/i, "No square brackets")
-  //.required()
-  //.max(500, "Max 500 characters"),
 });
 
 const SignupLoginForm = () => {
@@ -48,13 +43,14 @@ const SignupLoginForm = () => {
       venueManager: false,
     },
     validationSchema: validationSchema,
+
     onSubmit: (values) => {
       if (isSignUp) {
         // Handle signup form submission
         console.log("Registering user:", values);
       } else {
         // Handle login form submission
-        console.log("Logging in with:", values);
+        console.log("Logging in user:", values);
       }
     },
   });
@@ -63,7 +59,7 @@ const SignupLoginForm = () => {
     <>
       <form
         onSubmit={formik.handleSubmit}
-        className="mt-16 grid grid-cols-1 gap-y-6 gap-x-8 max-w-[800px] mx-auto px-2 "
+        className="my-2 grid grid-cols-1 gap-y-6 gap-x-8 max-w-[800px] mx-auto px-2 "
       >
         <div className="mt-4 text-center">
           <button
@@ -76,34 +72,178 @@ const SignupLoginForm = () => {
               : "New user? Click here to sign up a new account"}
           </button>
         </div>
+
         <div>
           {isSignUp && (
-            <div className="text-center">
-              <p className="block text-lg font-semibold leading-6 text-gray-900">
-                Sign up form:
+            <>
+              <p className="block text-lg text-center font-semibold leading-6 my-2 text-gray-900">
+                Sign up:
               </p>
-              <label
-                htmlFor="name"
-                className="block text-sm font-semibold leading-6 text-gray-900"
-              >
-                Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.name}
-                className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-              />
-              {formik.touched.name && formik.errors.name && (
-                <span className="text-red-600 text-md">
-                  Sorry, {formik.errors.name}. No special characters or spaces
-                  allowed except underscore, and minimum 3 characters
-                </span>
-              )}
-              <div className="sm:col-span-2">
+
+              <div className="text-start ">
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Name"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.name}
+                  className="block w-full rounded-md border-0 my-2 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                />
+                {formik.touched.name && formik.errors.name && (
+                  <span className="text-red-600 text-md">
+                    Sorry, {formik.errors.name}. No special characters or spaces
+                    allowed except underscore, and minimum 3 characters
+                  </span>
+                )}
+                <div className="sm:col-span-2">
+                  <label
+                    for="email"
+                    className="block text-sm font-semibold leading-6 text-gray-900"
+                  >
+                    Email
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      id="email"
+                      name="email"
+                      placeholder="Email"
+                      type="email"
+                      onChange={formik.handleChange}
+                      value={formik.values.email}
+                      autoComplete="email"
+                      className="block w-full rounded-md border-0 my-2 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                    />
+                    {formik.touched.email && formik.errors.email ? (
+                      <div className="text-red-600 text-md">
+                        Sorry, {formik.errors.email}, retry with a valid email
+                        address and no special characters
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                </div>
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-semibold leading-6 text-gray-900"
+                  >
+                    Password
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      type={showPassword ? "text" : "password"} // Toggle input type based on showPassword state
+                      password="password"
+                      placeholder="Password"
+                      onChange={formik.handleChange}
+                      value={formik.values.password}
+                      id="password"
+                      className="block w-full rounded-md border-0 my-2 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                    />
+                    <button
+                      type="button"
+                      onClick={() => setShowPassword(!showPassword)} // Toggle showPassword state on button click
+                      className="text-primary mt-1 text-sm"
+                    >
+                      {showPassword ? "Hide" : "Show"} password
+                    </button>
+                    {formik.touched.password && formik.errors.password ? (
+                      <div className="text-red-600 text-md">
+                        {formik.errors.password}
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                </div>
+
+                <div className="sm:col-span-2 ">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-semibold leading-6 text-gray-900"
+                  >
+                    Repeat Password
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      type={showPassword ? "text" : "password"} // Toggle input type based on showPassword state
+                      id="confirmPassword"
+                      name="confirmPassword"
+                      placeholder="Repeat password"
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      value={formik.values.confirmPassword}
+                      className="block w-full rounded-md border-0 my-2 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                    />
+                    <button
+                      type="button"
+                      onClick={() => setShowPassword(!showPassword)} // Toggle showPassword state on button click
+                      className="text-primary mt-1 text-sm"
+                    >
+                      {showPassword ? "Hide" : "Show"} password
+                    </button>
+                    {formik.touched.confirmPassword &&
+                    formik.errors.confirmPassword ? (
+                      <div className="text-red-600 text-md">
+                        {formik.errors.confirmPassword}
+                      </div>
+                    ) : null}
+                  </div>
+                </div>
+
+                <label
+                  htmlFor="avatarUrl"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Avatar URL
+                </label>
+                <input
+                  type="text"
+                  id="avatarUrl"
+                  name="avatarUrl"
+                  placeholder="Publically available image URL"
+                  onChange={formik.handleChange}
+                  value={formik.values.avatarUrl}
+                  className="block w-full rounded-md border-0 my-2 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                />
+                {formik.touched.avatarUrl && formik.errors.avatarUrl && (
+                  <div className="text-red-600 text-md">
+                    Sorry,{formik.errors.avatarUrl}. Url must be for publically
+                    accessible image and start with http:// or https://
+                  </div>
+                )}
+                <div>
+                  <label htmlFor="venueManager" className="mt-2">
+                    I want to sign up as a Holidaze venue manager
+                  </label>
+                  <input
+                    type="checkbox"
+                    id="venueManager"
+                    name="venueManager"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    checked={formik.values.venueManager}
+                    className="mx-1 my-4"
+                  />
+                </div>
+              </div>
+            </>
+          )}
+
+          {!isSignUp && (
+            <div>
+              <p className="block text-lg text-center font-semibold leading-6 my-2 text-gray-900">
+                Log in:
+              </p>
+              <div className="sm:col-span-2 ">
                 <label
                   for="email"
                   className="block text-sm font-semibold leading-6 text-gray-900"
@@ -118,7 +258,7 @@ const SignupLoginForm = () => {
                     onChange={formik.handleChange}
                     value={formik.values.email}
                     autoComplete="email"
-                    className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 my-2 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
                   />
                   {formik.touched.email && formik.errors.email ? (
                     <div className="text-red-600 text-md">
@@ -139,16 +279,16 @@ const SignupLoginForm = () => {
                 </label>
                 <div className="mt-2.5">
                   <input
-                    type={showPassword ? "text" : "password"} // Toggle input type based on showPassword state
+                    type={showPassword ? "text" : "password"}
                     password="password"
                     onChange={formik.handleChange}
                     value={formik.values.password}
                     id="password"
-                    className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 my-2 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
                   />
                   <button
                     type="button"
-                    onClick={() => setShowPassword(!showPassword)} // Toggle showPassword state on button click
+                    onClick={() => setShowPassword(!showPassword)}
                     className="text-primary mt-1 text-sm"
                   >
                     {showPassword ? "Hide" : "Show"} password
@@ -162,139 +302,13 @@ const SignupLoginForm = () => {
                   )}
                 </div>
               </div>
-
-              <div className="sm:col-span-2 ">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
-                >
-                  Repeat Password
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    type={showPassword ? "text" : "password"} // Toggle input type based on showPassword state
-                    id="confirmPassword"
-                    name="confirmPassword"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.confirmPassword}
-                    className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-                  />
-                  <button
-                    type="button"
-                    onClick={() => setShowPassword(!showPassword)} // Toggle showPassword state on button click
-                    className="text-primary mt-1 text-sm"
-                  >
-                    {showPassword ? "Hide" : "Show"} password
-                  </button>
-                  {formik.touched.confirmPassword &&
-                  formik.errors.confirmPassword ? (
-                    <div className="text-red-600 text-md">
-                      {formik.errors.confirmPassword}
-                    </div>
-                  ) : null}
-                </div>
-              </div>
-
-              <label
-                htmlFor="avatarUrl"
-                className="block text-sm font-semibold leading-6 text-gray-900"
-              >
-                Avatar URL
-              </label>
-              <input
-                type="text"
-                id="avatarUrl"
-                name="avatarUrl"
-                onChange={formik.handleChange}
-                value={formik.values.avatarUrl}
-                className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-              />
-              {formik.touched.avatarUrl && formik.errors.avatarUrl && (
-                <div className="text-red-600 text-md">
-                  Sorry,{formik.errors.avatarUrl}. Url must be for publically
-                  accessible image and start with http:// or https://
-                </div>
-              )}
-              <div>
-                <label htmlFor="venueManager">Venue Manager</label>
-                <input
-                  type="checkbox"
-                  id="venueManager"
-                  name="venueManager"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  checked={formik.values.venueManager}
-                />
-              </div>
             </div>
-          )}
-
-          {!isSignUp && (
-            <>
-              <div className="sm:col-span-2 ">
-                <label
-                  for="email"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
-                >
-                  Email LOGIN SIDE
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    onChange={formik.handleChange}
-                    value={formik.values.email}
-                    autoComplete="email"
-                    className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-                  />
-                  {formik.touched.email && formik.errors.email ? (
-                    <div>
-                      Sorry, {formik.errors.email}, retry with a valid email
-                      address and no special characters
-                    </div>
-                  ) : (
-                    ""
-                  )}
-                </div>
-              </div>
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
-                >
-                  Password LOGIN SIDE
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    type={showPassword ? "text" : "password"} // Toggle input type based on showPassword state
-                    password="password"
-                    onChange={formik.handleChange}
-                    value={formik.values.password}
-                    id="password"
-                    className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-                  />
-                  <button
-                    type="button"
-                    onClick={() => setShowPassword(!showPassword)} // Toggle showPassword state on button click
-                    className="text-gray-600 mt-1 text-sm"
-                  >
-                    {showPassword ? "Hide" : "Show"} password
-                  </button>
-                  {formik.touched.password && formik.errors.password ? (
-                    <div>{formik.errors.password}</div>
-                  ) : (
-                    ""
-                  )}
-                </div>
-              </div>
-            </>
           )}
         </div>
 
         <div className="flex justify-end border-t border-gray-900/10 ">
           <button
+            onClick={console.log("button clicked")}
             type="submit"
             className="mx-auto rounded-md bg-blue-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
           >
